80cfa219cd7b89ac96cdb52120e75dec52954fcc
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2023-11-18 13:11:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 //your JS code here. If required.
 function functionName() {
-    alert("functionName");
+   alert(arguments.callee.name);
+}
+function AccioJob() {
+   alert(arguments.callee.name);
 }
 
 // Calling the function will display its own name
 functionName();
+AccioJob();
